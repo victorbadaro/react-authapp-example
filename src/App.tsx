a37558e5b03515a8276d6d1818cc563cce077576
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-29 17:24:40 -0300

    feat: add app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRoutes } from './Routes';
 import { AuthProvider } from './context/AuthContext';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <AuthProvider>
+      <GlobalStyle />
       <Router>
         <AppRoutes />
       </Router>
