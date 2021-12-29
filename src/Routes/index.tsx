62f6d43b9444be81a5a6319ae3f57aaefde793ff
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-29 17:01:32 -0300

    feat: add routes for Login, Profile, Products and NotFound pages

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,9 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { Login } from '../pages/Login';
+import { Profile } from '../pages/Profile';
+import { Products } from '../pages/Products';
+import { NotFound } from '../pages/NotFound';
 
 export function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" />
+            <Route path="/" element={<Login />} />
+            
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/products" element={<Products />} />
+
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
