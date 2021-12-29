72b69651c5aa30b71931644f18758cf34e672f3b
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-29 17:45:32 -0300

    feat: check for authentication on localStorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 
 interface AuthContextProps {
     authenticated: boolean;
@@ -14,18 +14,30 @@ const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
 function AuthProvider({ children }: AuthProviderProps) {
     const [authenticated, setAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const storagedAuthentication = localStorage.getItem('authenticated');
+
+        if(storagedAuthentication)
+            setAuthenticated(true);
+        
+        setLoading(false);
+    }, []);
 
     function signIn() {
+        localStorage.setItem('authenticated', JSON.stringify(true));
         setAuthenticated(true);
     }
 
     function signOut() {
+        localStorage.removeItem('authenticated');
         setAuthenticated(false);
     }
 
     return (
         <AuthContext.Provider value={{ authenticated, signIn, signOut }}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     );
 }
